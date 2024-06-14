a71ed8cbc3ac5b03e1e105d48535d5034b367345
deep-patel21 <deepstudios21@gmail.com>
2024-06-14 11:59:02 -0400

    Initial file setup.

---
+++ src/backend/video.py
@@ -0,0 +1,19 @@
+"""
+@file: video.py
+@directory: /Automated-Dashcam-Montages/src/backend/
+
+The purpose of this file is to navigate through dashcam video backup
+directories and retreive all videos corresponding to a particular trip.
+These clips will be stitched together to send to the frontend for display.
+"""
+
+import moviepy.editor as mp
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
